389636507742af9a6e2718cfb9133c3e82d1cc3d
Daniele Pantaleone <danielepantaleone@me.com>
2015-12-18 00:19:22 +0100

    bumpversion: 0.5

--- eddy/__init__.py
+++ eddy/__init__.py
@@ -38,7 +38,7 @@ __email__ = 'danielepantaleone@me.com'
 __copyright__ = 'Copyright © 2015 Daniele Pantaleone'
 __organization__ = 'Sapienza - University Of Rome'
 __appname__ = 'Eddy'
-__version__ = '0.4'
+__version__ = '0.5'
 __status__ = 'Development'
 __license__ = 'GPL'
 
