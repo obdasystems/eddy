c1276d510f8b3f575c81e6a8afb1248acb74c855
Daniele Pantaleone <danielepantaleone@me.com>
2016-01-08 17:25:41 +0100

    regex: added regular expressions to parse JVM home and version number

--- eddy/core/regex.py
+++ eddy/core/regex.py
@@ -35,7 +35,9 @@
 import re
 
 
+RE_CARDINALITY = re.compile("""^\(\s*(?P<min>[\d-]+)\s*,\s*(?P<max>[\d-]+)\s*\)$""") # parse cardinality restriction
 RE_DIGIT = re.compile("""\d""") # identify strings composed of only digits
 RE_ITEM_PREFIX = re.compile("""^(?P<prefix>[^\d])(?P<value>\d+)$""") # split items prefix/id
-RE_CARDINALITY = re.compile("""^\(\s*(?P<min>[\d-]+)\s*,\s*(?P<max>[\d-]+)\s*\)$""") # parse cardinality restriction
+RE_JAVA_HOME = re.compile("""^(jvm|jdk|jre)((?P<major>\d)\.?)?((?P<minor>\d)\.?)?((?P<patch>\d)_?)?(?P<update>\d{1,3})?(\.*\w*)?$""")
+RE_JAVA_VERSION = re.compile("""^((?P<major>\d)\.?)((?P<minor>\d)\.?)((?P<patch>\d)_?)?(?P<update>\d{1,3})$""")
 RE_QUOTE_FULL = re.compile("""^".*"$""") # identify strings fully embraced into quotes
