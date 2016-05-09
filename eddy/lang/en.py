fbe4a0e7ee264945f71914a8adbadb45c2b74874
Daniele Pantaleone <danielepantaleone@me.com>
2016-05-09 13:39:51 +0200

    lang: rearrange english translation

--- eddy/lang/en.py
+++ eddy/lang/en.py
@@ -90,26 +90,6 @@ ACTION_SNAP_TO_GRID_S = 'Snap diagram elements to the grid'
 ACTION_SYNTAX_CHECK_S = 'Run syntax validation on the current project'
 
 #############################################
-# MENUS
-#################################
-
-MENU_COMPOSE = 'Compose'
-MENU_EDIT = 'Edit'
-MENU_FILE = 'File'
-MENU_HELP = 'Help'
-MENU_REFACTOR = 'Refactor'
-MENU_REFACTOR_BRUSH = 'Select color'
-MENU_SET_BRUSH = 'Select color'
-MENU_SET_DATATYPE = 'Select type'
-MENU_SET_FACET = 'Select facet'
-MENU_SET_INDIVIDUAL_AS = 'Set as'
-MENU_SET_PROPERTY_RESTRICTION = 'Select restriction'
-MENU_SET_SPECIAL = 'Special type'
-MENU_SWITCH_OPERATOR = 'Switch to'
-MENU_TOOLS = 'Tools'
-MENU_VIEW = 'View'
-
-#############################################
 # UNDO / REDO COMMANDS
 #################################
 
@@ -152,42 +132,31 @@ COMMAND_NODE_SET_VALUE = 'change value to {0}'
 COMMAND_NODE_SET_VALUE_RESTRICTION = 'change value restriction to {0}'
 
 #############################################
-# SYNTAX VALIDATION
+# DIAGRAM CREATE / EDIT / LOAD
 #################################
 
-SYNTAX_SELF_CONNECTION = 'Self connection is not valid'
+DIAGRAM_CAPTION_ALREADY_EXISTS = "Diagram '{0}' already exists!"
+DIAGRAM_CAPTION_NAME_NOT_VALID = "'{0}' is not a valid diagram name!"
+DIAGRAM_CREATION_FAILED_WINDOW_TITLE = 'Diagram creation failed!'
+DIAGRAM_CREATION_FAILED_MESSAGE = 'Eddy could not create the specified diagram: {0}!'
+DIAGRAM_LOAD_FAILED_WINDOW_TITLE = 'Diagram load failed!'
+DIAGRAM_LOAD_FAILED_MESSAGE = 'Eddy could not load the specified diagram: {0}!'
+DIAGRAM_LOCATION_LABEL = 'Location'
+DIAGRAM_NAME_LABEL = 'Name'
+DIAGRAM_WINDOW_TITLE_NEW = 'New diagram'
 
-SYNTAX_INCLUSION_CHAIN_AS_TARGET = 'Role chain nodes cannot be target of a Role inclusion'
-SYNTAX_INCLUSION_CHAIN_AS_SOURCE_INVALID_TARGET = 'Inclusion between {0} and {0} is forbidden'
-SYNTAX_INCLUSION_COMPLEMENT_INVALID_SOURCE = 'Invalid source for {0} inclusion: {1}'
-SYNTAX_INCLUSION_NO_GRAPHOL_EXPRESSION = 'Type mismatch: inclusion must involve two graphol expressions'
-SYNTAX_INCLUSION_NOT_COMPATIBLE = 'Type mismatch: {0} and {1} are not compatible'
-SYNTAX_INCLUSION_TYPE_MISMATCH = 'Type mismatch: inclusion between {0} and {1}'
-SYNTAX_INCLUSION_VALUE_DOMAIN_MISSING_ATOMIC = 'Inclusion between value-domain expressions must include at least an atomic datatype'
+#############################################
+# DIAGRAM IMPORT
+#################################
 
-SYNTAX_INPUT_COMPLEMENT_INVALID_EXPRESSION = 'Invalid negative {0} expression'
-SYNTAX_INPUT_DATA_TOO_MANY_DATATYPE = 'Too many value-domain nodes in input to datatype restriction node'
-SYNTAX_INPUT_DATA_INVALID_FACET = 'Type mismatch: facet {0} is not supported by {1} datatype'
-SYNTAX_INPUT_DR_INVALID_QUALIFIED_RESTRICTION = 'Invalid restriction ({0}) for qualified restriction'
-SYNTAX_INPUT_DR_INVALID_QUALIFIED_RESTRICTION_OPERANDS = 'Invalid inputs ({0} + {1}) for qualified restriction'
-SYNTAX_INPUT_DR_ATTRIBUTE_NO_SELF = 'Attributes do not have self'
-SYNTAX_INPUT_ENUMERATION_INVALID_TARGET_IDENTITY = 'Target node has an invalid identity: {0}'
-SYNTAX_INPUT_PROP_ASSERTION_TOO_MANY_INSTANCES = 'Too many instances in input to {0}'
-SYNTAX_INPUT_PROP_ASSERTION_TOO_MANY_VALUES = 'Too many values in input to {0}'
-SYNTAX_INPUT_INVALID_COMPOSITION = 'Type mismatch: {0} between {1} and {2}'
-SYNTAX_INPUT_INVALID_OPERAND = 'Invalid input to {0}: {1}'
-SYNTAX_INPUT_INVALID_TARGET = 'Input edges can only target constructor nodes'
-SYNTAX_INPUT_TOO_MANY_OPERANDS = 'Too many inputs to {0}'
-
-SYNTAX_MEMBERSHIP_INVALID_ASSERTION_TARGET = 'Invalid target for {0} assertion: {1}'
-SYNTAX_MEMBERSHIP_INVALID_SOURCE = 'Invalid source for membership edge: {0}'
-SYNTAX_MEMBERSHIP_INVALID_TARGET = 'Invalid target for membership edge: {0}'
-
-SYNTAX_MANUAL_EDGE_ERROR = 'Syntax error detected on {0} from {1} to {2}: <i>{3}</i>.'
-SYNTAX_MANUAL_NODE_IDENTITY_UNKNOWN = 'Unkown node identity detected on {0}.'
-SYNTAX_MANUAL_NO_ERROR_FOUND = 'No syntax error found!'
-SYNTAX_MANUAL_PROGRESS_TITLE = 'Running syntax validation...'
-SYNTAX_MANUAL_WINDOW_TITLE = 'Syntax validation completed!'
+DIAGRAM_IMPORT_FAILED_WINDOW_TITLE = 'Diagram import failed!'
+DIAGRAM_IMPORT_FAILED_MESSAGE = 'Eddy could not import the specified file: {0}!'
+DIAGRAM_IMPORT_PROGRESS_TITLE = 'Importing {0}...'
+DIAGRAM_IMPORT_PARTIAL_WINDOW_TITLE = 'Partial document import!'
+DIAGRAM_IMPORT_PARTIAL_INFORMATIVE_MESSAGE = 'If needed, <a href="{0}">submit a bug report</a> with detailed information.'
+DIAGRAM_IMPORT_PARTIAL_MESSAGE = 'Document {0} has been imported! However some errors ({1}) have ' \
+                                 'been generated during the import process. You can inspect detailed ' \
+                                 'information by expanding the box below.'
 
 #############################################
 # DOCK WIDGETS
@@ -198,12 +167,6 @@ DOCK_INFO = 'Info'
 DOCK_OVERVIEW = 'Overview'
 DOCK_PALETTE = 'Palette'
 DOCK_PROJECT_EXPLORER = 'Project Explorer'
-
-#############################################
-# ONTOLOGY EXPLORER
-#################################
-
-ONTO_EXPLORER_SEARCH_PLACEHOLDER = 'Search...'
 
 #############################################
 # INFO BOX
@@ -242,31 +205,30 @@ INFO_KEY_TYPE = 'Type'
 INFO_KEY_VALUE = 'Value'
 
 #############################################
-# DIAGRAM CREATE / EDIT / LOAD
+# MENUS
 #################################
 
-DIAGRAM_CAPTION_ALREADY_EXISTS = "Diagram '{0}' already exists!"
-DIAGRAM_CAPTION_NAME_NOT_VALID = "'{0}' is not a valid diagram name!"
-DIAGRAM_CREATION_FAILED_WINDOW_TITLE = 'Diagram creation failed!'
-DIAGRAM_CREATION_FAILED_MESSAGE = 'Eddy could not create the specified diagram: {0}!'
-DIAGRAM_LOAD_FAILED_WINDOW_TITLE = 'Diagram load failed!'
-DIAGRAM_LOAD_FAILED_MESSAGE = 'Eddy could not load the specified diagram: {0}!'
-DIAGRAM_LOCATION_LABEL = 'Location'
-DIAGRAM_NAME_LABEL = 'Name'
-DIAGRAM_WINDOW_TITLE_NEW = 'New diagram'
+MENU_COMPOSE = 'Compose'
+MENU_EDIT = 'Edit'
+MENU_FILE = 'File'
+MENU_HELP = 'Help'
+MENU_REFACTOR = 'Refactor'
+MENU_REFACTOR_BRUSH = 'Select color'
+MENU_SET_BRUSH = 'Select color'
+MENU_SET_DATATYPE = 'Select type'
+MENU_SET_FACET = 'Select facet'
+MENU_SET_INDIVIDUAL_AS = 'Set as'
+MENU_SET_PROPERTY_RESTRICTION = 'Select restriction'
+MENU_SET_SPECIAL = 'Special type'
+MENU_SWITCH_OPERATOR = 'Switch to'
+MENU_TOOLS = 'Tools'
+MENU_VIEW = 'View'
 
 #############################################
-# DIAGRAM IMPORT
+# ONTOLOGY EXPLORER
 #################################
 
-DIAGRAM_IMPORT_FAILED_WINDOW_TITLE = 'Diagram import failed!'
-DIAGRAM_IMPORT_FAILED_MESSAGE = 'Eddy could not import the specified file: {0}!'
-DIAGRAM_IMPORT_PROGRESS_TITLE = 'Importing {0}...'
-DIAGRAM_IMPORT_PARTIAL_WINDOW_TITLE = 'Partial document import!'
-DIAGRAM_IMPORT_PARTIAL_INFORMATIVE_MESSAGE = 'If needed, <a href="{0}">submit a bug report</a> with detailed information.'
-DIAGRAM_IMPORT_PARTIAL_MESSAGE = 'Document {0} has been imported! However some errors ({1}) have ' \
-                                 'been generated during the import process. You can inspect detailed ' \
-                                 'information by expanding the box below.'
+ONTO_EXPLORER_SEARCH_PLACEHOLDER = 'Search...'
 
 #############################################
 # PROJECT DIALOG
@@ -314,6 +276,44 @@ PROJECT_EXPORT_OWL_UNSUPPORTED_SYNTAX = 'unsupported syntax ({0})'
 REFACTOR_NAME_RENAME_LABEL = 'Name'
 REFACTOR_NAME_WINDOW_TITLE = 'Rename'
 REFACTOR_NAME_CAPTION_NAME_NOT_VALID = "\'{0}\' is not a valid predicate name"
+
+#############################################
+# SYNTAX VALIDATION
+#################################
+
+SYNTAX_SELF_CONNECTION = 'Self connection is not valid'
+
+SYNTAX_INCLUSION_CHAIN_AS_TARGET = 'Role chain nodes cannot be target of a Role inclusion'
+SYNTAX_INCLUSION_CHAIN_AS_SOURCE_INVALID_TARGET = 'Inclusion between {0} and {0} is forbidden'
+SYNTAX_INCLUSION_COMPLEMENT_INVALID_SOURCE = 'Invalid source for {0} inclusion: {1}'
+SYNTAX_INCLUSION_NO_GRAPHOL_EXPRESSION = 'Type mismatch: inclusion must involve two graphol expressions'
+SYNTAX_INCLUSION_NOT_COMPATIBLE = 'Type mismatch: {0} and {1} are not compatible'
+SYNTAX_INCLUSION_TYPE_MISMATCH = 'Type mismatch: inclusion between {0} and {1}'
+SYNTAX_INCLUSION_VALUE_DOMAIN_MISSING_ATOMIC = 'Inclusion between value-domain expressions must include at least an atomic datatype'
+
+SYNTAX_INPUT_COMPLEMENT_INVALID_EXPRESSION = 'Invalid negative {0} expression'
+SYNTAX_INPUT_DATA_TOO_MANY_DATATYPE = 'Too many value-domain nodes in input to datatype restriction node'
+SYNTAX_INPUT_DATA_INVALID_FACET = 'Type mismatch: facet {0} is not supported by {1} datatype'
+SYNTAX_INPUT_DR_INVALID_QUALIFIED_RESTRICTION = 'Invalid restriction ({0}) for qualified restriction'
+SYNTAX_INPUT_DR_INVALID_QUALIFIED_RESTRICTION_OPERANDS = 'Invalid inputs ({0} + {1}) for qualified restriction'
+SYNTAX_INPUT_DR_ATTRIBUTE_NO_SELF = 'Attributes do not have self'
+SYNTAX_INPUT_ENUMERATION_INVALID_TARGET_IDENTITY = 'Target node has an invalid identity: {0}'
+SYNTAX_INPUT_PROP_ASSERTION_TOO_MANY_INSTANCES = 'Too many instances in input to {0}'
+SYNTAX_INPUT_PROP_ASSERTION_TOO_MANY_VALUES = 'Too many values in input to {0}'
+SYNTAX_INPUT_INVALID_COMPOSITION = 'Type mismatch: {0} between {1} and {2}'
+SYNTAX_INPUT_INVALID_OPERAND = 'Invalid input to {0}: {1}'
+SYNTAX_INPUT_INVALID_TARGET = 'Input edges can only target constructor nodes'
+SYNTAX_INPUT_TOO_MANY_OPERANDS = 'Too many inputs to {0}'
+
+SYNTAX_MEMBERSHIP_INVALID_ASSERTION_TARGET = 'Invalid target for {0} assertion: {1}'
+SYNTAX_MEMBERSHIP_INVALID_SOURCE = 'Invalid source for membership edge: {0}'
+SYNTAX_MEMBERSHIP_INVALID_TARGET = 'Invalid target for membership edge: {0}'
+
+SYNTAX_MANUAL_EDGE_ERROR = 'Syntax error detected on {0} from {1} to {2}: <i>{3}</i>.'
+SYNTAX_MANUAL_NODE_IDENTITY_UNKNOWN = 'Unkown node identity detected on {0}.'
+SYNTAX_MANUAL_NO_ERROR_FOUND = 'No syntax error found!'
+SYNTAX_MANUAL_PROGRESS_TITLE = 'Running syntax validation...'
+SYNTAX_MANUAL_WINDOW_TITLE = 'Syntax validation completed!'
 
 #############################################
 # WELCOME SCREEN
