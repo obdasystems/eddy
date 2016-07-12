b4ffcd6bb869cdeb174d259dee9f7d7cef2a2756
danielepantaleone <danielepantaleone@me.com>
2016-07-12 17:41:30 +0200

    core: added logging capabilities

---
+++ eddy/core/output.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+##########################################################################
+#                                                                        #
+#  Eddy: a graphical editor for the specification of Graphol ontologies  #
+#  Copyright (C) 2015 Daniele Pantaleone <pantaleone@dis.uniroma1.it>    #
+#                                                                        #
+#  This program is free software: you can redistribute it and/or modify  #
+#  it under the terms of the GNU General Public License as published by  #
+#  the Free Software Foundation, either version 3 of the License, or     #
+#  (at your option) any later version.                                   #
+#                                                                        #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
+#  GNU General Public License for more details.                          #
+#                                                                        #
+#  You should have received a copy of the GNU General Public License     #
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.  #
+#                                                                        #
+#  #####################                          #####################  #
+#                                                                        #
+#  Graphol is developed by members of the DASI-lab group of the          #
+#  Dipartimento di Ingegneria Informatica, Automatica e Gestionale       #
+#  A.Ruberti at Sapienza University of Rome: http://www.dis.uniroma1.it  #
+#                                                                        #
+#     - Domenico Lembo <lembo@dis.uniroma1.it>                           #
+#     - Valerio Santarelli <santarelli@dis.uniroma1.it>                  #
+#     - Domenico Fabio Savo <savo@dis.uniroma1.it>                       #
+#     - Daniele Pantaleone <pantaleone@dis.uniroma1.it>                  #
+#     - Marco Console <console@dis.uniroma1.it>                          #
+#                                                                        #
+##########################################################################
+
+
+import logging
+import sys
+
+from eddy import APPNAME
+
+
+__output = dict()
+
+
+def getLogger(name=APPNAME):
+    """
+    Returns an instance of the logger for the given name.
+    :type name: str
+    :rtype: logging.Logger
+    """
+    global __output
+    if not name in __output:
+
+        formatter = logging.Formatter(fmt='{asctime} [{levelname}] {message}', datefmt='%Y/%m/%d %H:%M:%S', style='{')
+        handler = logging.StreamHandler(sys.stderr)
+        handler.setFormatter(formatter)
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.DEBUG)
+        logger.addHandler(handler)
+
+        __output[name] = logger
+
+    return __output.get(name)
