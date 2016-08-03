1a985c9ea5a899c1131eacecff1ebb96c71e86a5
danielepantaleone <danielepantaleone@me.com>
2016-08-03 20:01:35 +0200

    core: added HasPluginSystem and HasWidgetSystem mixins

--- eddy/core/common.py
+++ eddy/core/common.py
@@ -82,7 +82,7 @@ class HasActionSystem(object):
             raise ValueError("duplicate action found: %s" % action.objectName())
         self._actionList.append(action)
         self._actionDict[action.objectName()] = action
-        LOGGER.debug("Added %s(%s)", action.__class__.__name__, action.objectName())
+        # LOGGER.debug("Added %s(%s)", action.__class__.__name__, action.objectName())
         return action
 
     def addActions(self, actions):
@@ -106,9 +106,9 @@ class HasActionSystem(object):
             raise ValueError("duplicate action found: %s" % action.objectName())
         self._actionList.insert(self._actionList.index(before), action)
         self._actionDict[action.objectName()] = action
-        LOGGER.debug("Added %s(%s) before %s(%s)",
-                     action.__class__.__name__, action.objectName(),
-                     before.__class__.__name__, before.objectName())
+        # LOGGER.debug("Added %s(%s) before %s(%s)",
+        #              action.__class__.__name__, action.objectName(),
+        #              before.__class__.__name__, before.objectName())
         return action
 
     def insertActions(self, actions, before):
@@ -156,7 +156,7 @@ class HasMenuSystem(object):
             raise ValueError("duplicate menu found: %s" % menu.objectName())
         self._menuList.append(menu)
         self._menuDict[menu.objectName()] = menu
-        LOGGER.debug("Added %s(%s)", menu.__class__.__name__, menu.objectName())
+        # LOGGER.debug("Added %s(%s)", menu.__class__.__name__, menu.objectName())
         return menu
 
     def addMenus(self, menus):
@@ -180,9 +180,9 @@ class HasMenuSystem(object):
             raise ValueError("duplicate menu found: %s" % menu.objectName())
         self._menuList.insert(self._menuList.index(before), menu)
         self._menuDict[menu.objectName()] = menu
-        LOGGER.debug("Added %s(%s) before %s(%s)",
-                     menu.__class__.__name__, menu.objectName(),
-                     before.__class__.__name__, before.objectName())
+        # LOGGER.debug("Added %s(%s) before %s(%s)",
+        #              menu.__class__.__name__, menu.objectName(),
+        #              before.__class__.__name__, before.objectName())
         return menu
 
     def insertMenus(self, menus, before):
@@ -218,3 +218,181 @@ class HasMenuSystem(object):
         self._menuList.remove(menu)
         del self._menuDict[menu.objectName()]
         return menu
+
+
+class HasPluginSystem(object):
+    """
+    Mixin which adds the ability to store and retrieve plugins.
+    """
+    def __init__(self, **kwargs):
+        """
+        Initialize the object with default parameters.
+        :type kwargs: dict
+        """
+        super().__init__(**kwargs)
+        self._pluginDict = {}
+        self._pluginList = []
+
+    def addPlugin(self, plugin):
+        """
+        Add a plugin to the set.
+        :type plugin: AbstractPlugin
+        :rtype: AbstractPlugin
+        """
+        if isEmpty(plugin.objectName()):
+            raise ValueError("missing objectName in %s" % plugin.__class__.__name__)
+        if plugin.objectName() in self._pluginDict:
+            raise ValueError("duplicate plugin found: %s" % plugin.objectName())
+        self._pluginList.append(plugin)
+        self._pluginDict[plugin.objectName()] = plugin
+        # LOGGER.debug("Added %s(%s)", plugin.__class__.__name__, plugin.objectName())
+        return plugin
+
+    def addPlugins(self, plugins):
+        """
+        Add the given group of plugins to the set.
+        :type plugins: T <= list|tuple
+        """
+        for plugin in plugins:
+            self.addPlugin(plugin)
+
+    def insertPlugin(self, widget, before):
+        """
+        Insert the given plugin before the given one.
+        :type widget: AbstractPlugin
+        :type before: AbstractPlugin
+        :rtype: AbstractPlugin
+        """
+        if isEmpty(widget.objectName()):
+            raise ValueError("missing objectName in %s" % widget.__class__.__name__)
+        if widget.objectName() in self._pluginDict:
+            raise ValueError("duplicate plugin found: %s" % widget.objectName())
+        self._pluginList.insert(self._pluginList.index(before), widget)
+        self._pluginDict[widget.objectName()] = widget
+        # LOGGER.debug("Added %s(%s) before %s(%s)",
+        #              plugin.__class__.__name__, plugin.objectName(),
+        #              before.__class__.__name__, before.objectName())
+        return widget
+
+    def insertPlugins(self, plugins, before):
+        """
+        Insert the given group of plugins before the given one.
+        :type plugins: T <= list|tuple
+        :type before: AbstractPlugin
+        """
+        for plugin in plugins:
+            self.insertPlugin(plugin, before)
+
+    def plugin(self, objectName):
+        """
+        Returns the reference to a plugin given it's objectName.
+        :type objectName: str
+        :rtype: AbstractPlugin
+        """
+        return self._pluginDict.get(objectName, None)
+
+    def plugins(self):
+        """
+        Returns the list of plugins.
+        :rtype: list
+        """
+        return self._pluginList
+
+    def removePlugin(self, plugin):
+        """
+        Removes the given plugin from the set.
+        :type plugin: AbstractPlugin
+        :rtype: AbstractPlugin
+        """
+        self._pluginList.remove(plugin)
+        del self._pluginDict[plugin.objectName()]
+        return plugin
+
+
+class HasWidgetSystem(object):
+    """
+    Mixin which adds the ability to store and retrieve widgets.
+    """
+    def __init__(self, **kwargs):
+        """
+        Initialize the object with default parameters.
+        :type kwargs: dict
+        """
+        super().__init__(**kwargs)
+        self._widgetDict = {}
+        self._widgetList = []
+
+    def addWidget(self, widget):
+        """
+        Add a QWidget to the set.
+        :type widget: QWidget
+        :rtype: QWidget
+        """
+        if isEmpty(widget.objectName()):
+            raise ValueError("missing objectName in %s" % widget.__class__.__name__)
+        if widget.objectName() in self._widgetDict:
+            raise ValueError("duplicate widget found: %s" % widget.objectName())
+        self._widgetList.append(widget)
+        self._widgetDict[widget.objectName()] = widget
+        # LOGGER.debug("Added %s(%s)", widget.__class__.__name__, widget.objectName())
+        return widget
+
+    def addWidgets(self, widgets):
+        """
+        Add the given group of QWidget to the set.
+        :type widgets: T <= list|tuple
+        """
+        for widget in widgets:
+            self.addWidget(widget)
+
+    def insertWidget(self, widget, before):
+        """
+        Insert the given QWidget before the given one.
+        :type widget: QWidget
+        :type before: QWidget
+        :rtype: QWidget
+        """
+        if isEmpty(widget.objectName()):
+            raise ValueError("missing objectName in %s" % widget.__class__.__name__)
+        if widget.objectName() in self._widgetDict:
+            raise ValueError("duplicate widget found: %s" % widget.objectName())
+        self._widgetList.insert(self._widgetList.index(before), widget)
+        self._widgetDict[widget.objectName()] = widget
+        # LOGGER.debug("Added %s(%s) before %s(%s)",
+        #              widget.__class__.__name__, widget.objectName(),
+        #              before.__class__.__name__, before.objectName())
+        return widget
+
+    def insertWidgets(self, widgets, before):
+        """
+        Insert the given group of QWidget before the given one.
+        :type widgets: T <= list|tuple
+        :type before: QWidget
+        """
+        for widget in widgets:
+            self.insertWidget(widget, before)
+
+    def widget(self, objectName):
+        """
+        Returns the reference to a QWidget given it's objectName.
+        :type objectName: str
+        :rtype: QWidget
+        """
+        return self._widgetDict.get(objectName, None)
+
+    def widgets(self):
+        """
+        Returns the list of QWidget.
+        :rtype: list
+        """
+        return self._widgetList
+
+    def removeWidget(self, widget):
+        """
+        Removes the given QWidget from the set.
+        :type widget: QWidget
+        :rtype: QWidget
+        """
+        self._widgetList.remove(widget)
+        del self._widgetDict[widget.objectName()]
+        return widget
