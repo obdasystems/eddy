dc9920cd418b982996983bb862ec531505b0d7a1
danielepantaleone <danielepantaleone@me.com>
2016-09-15 18:57:44 +0200

    tests: updated travis and circle configurations

--- scripts/configure-circle.sh
+++ scripts/configure-circle.sh
@@ -41,24 +41,24 @@ cd ~
 mkdir "${BUILDS_DIR}"
 
 # Install Qt5
-sudo apt-add-repository ppa:beineri/opt-qt551 -y
+sudo apt-add-repository ppa:beineri/opt-qt57-trusty -y
 sudo apt-get update
 sudo apt-get install qt-latest
 sudo apt-get install mercurial
-source /opt/qt55/bin/qt55-env.sh
+source /opt/qt57/bin/qt57-env.sh
 
 # Install Sip
-curl -L -o "${BUILDS_DIR}/sip.tar.gz" "http://downloads.sourceforge.net/project/pyqt/sip/sip-4.18/sip-4.18.tar.gz"
+curl -L -o "${BUILDS_DIR}/sip.tar.gz" "http://downloads.sourceforge.net/project/pyqt/sip/sip-4.18.1/sip-4.18.1.tar.gz"
 tar xzf "${BUILDS_DIR}/sip.tar.gz" -C "${BUILDS_DIR}" --keep-newer-files
-cd "${BUILDS_DIR}/sip-4.18"
+cd "${BUILDS_DIR}/sip-4.18.1"
 python configure.py
 make
 sudo make install
 
 # Install PyQt5
-curl -L -o "${BUILDS_DIR}/pyqt.tar.gz" "http://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.5.1/PyQt-gpl-5.5.1.tar.gz"
+curl -L -o "${BUILDS_DIR}/pyqt.tar.gz" "http://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.7/PyQt5_gpl-5.7.tar.gz"
 tar xzf "${BUILDS_DIR}/pyqt.tar.gz" -C "${BUILDS_DIR}" --keep-newer-files
-cd "${BUILDS_DIR}/PyQt-gpl-5.5.1"
+cd "${BUILDS_DIR}/PyQt5_gpl-5.7"
 python configure.py --confirm-license --no-designer-plugin -e QtCore -e QtGui -e QtWidgets -e QtPrintSupport -e QtXml -e QtTest -e QtNetwork
 make
 sudo make install
@@ -66,6 +66,7 @@ sudo make install
 # Install PIP packages
 pip install -U pip
 pip install -U cython==0.23.4
+pip install -U verlib==0.1
 pip install -U mockito-without-hardcoded-distribute-version==0.5.4
 pip install -U nose==1.3.7
 pip install -U nose-cov==1.6
--- scripts/configure-travis.sh
+++ scripts/configure-travis.sh
@@ -41,11 +41,11 @@ cd ~
 mkdir "${BUILDS_DIR}"
 
 # Install Qt5
-sudo apt-add-repository ppa:beineri/opt-qt551 -y
+sudo apt-add-repository ppa:beineri/opt-qt57-trusty -y
 sudo apt-get update
 sudo apt-get install qt-latest
 sudo apt-get install mercurial
-source /opt/qt55/bin/qt55-env.sh
+source /opt/qt57/bin/qt57-env.sh
 
 # Install OracleJDK8
 sudo add-apt-repository ppa:webupd8team/java -y
@@ -54,17 +54,17 @@ sudo apt-get install oracle-java8-installer
 sudo apt-get install oracle-java8-set-default
 
 # Install Sip
-curl -L -o "${BUILDS_DIR}/sip.tar.gz" "http://downloads.sourceforge.net/project/pyqt/sip/sip-4.18/sip-4.18.tar.gz"
+curl -L -o "${BUILDS_DIR}/sip.tar.gz" "http://downloads.sourceforge.net/project/pyqt/sip/sip-4.18.1/sip-4.18.1.tar.gz"
 tar xzf "${BUILDS_DIR}/sip.tar.gz" -C "${BUILDS_DIR}" --keep-newer-files
-cd "${BUILDS_DIR}/sip-4.18"
+cd "${BUILDS_DIR}/sip-4.18.1"
 python configure.py
 make
 sudo make install
 
 # Install PyQt5
-curl -L -o "${BUILDS_DIR}/pyqt.tar.gz" "http://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.5.1/PyQt-gpl-5.5.1.tar.gz"
+curl -L -o "${BUILDS_DIR}/pyqt.tar.gz" "http://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.7/PyQt5_gpl-5.7.tar.gz"
 tar xzf "${BUILDS_DIR}/pyqt.tar.gz" -C "${BUILDS_DIR}" --keep-newer-files
-cd "${BUILDS_DIR}/PyQt-gpl-5.5.1"
+cd "${BUILDS_DIR}/PyQt5_gpl-5.7"
 python configure.py --confirm-license --no-designer-plugin -e QtCore -e QtGui -e QtWidgets -e QtPrintSupport -e QtXml -e QtTest -e QtNetwork
 make
 sudo make install
@@ -72,6 +72,7 @@ sudo make install
 # Install PIP packages
 pip install -U pip
 pip install -U cython==0.23.4
+pip install -U verlib==0.1
 pip install -U mockito-without-hardcoded-distribute-version==0.5.4
 pip install -U nose==1.3.7
 pip install -U nose-cov==1.6
