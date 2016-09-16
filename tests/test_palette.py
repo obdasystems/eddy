6a3e8699c9b8ce7b742fafa8adff210c209a20f5
danielepantaleone <danielepantaleone@me.com>
2016-09-16 09:45:09 +0200

    travis: skip oracle jdk8 installation

--- tests/test_palette.py
+++ tests/test_palette.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
 
 ##########################################################################
 #                                                                        #
@@ -33,52 +33,13 @@
 ##########################################################################
 
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PARENT_DIR="$(dirname "${SCRIPT_DIR}")"
-BUILDS_DIR="${PARENT_DIR}/builds"
+from tests import EddyTestCase
 
-cd ~
-mkdir "${BUILDS_DIR}"
 
-# Install Qt5
-sudo apt-add-repository ppa:beineri/opt-qt57-trusty -y
-sudo apt-get update
-sudo apt-get install qt-latest
-sudo apt-get install mercurial
-source /opt/qt57/bin/qt57-env.sh
-
-# Install OracleJDK8
-#sudo add-apt-repository ppa:webupd8team/java -y
-#sudo apt-get update
-#sudo apt-get install oracle-java8-installer -y
-#sudo apt-get install oracle-java8-set-default -y
-
-# Install Sip
-curl -L -o "${BUILDS_DIR}/sip.tar.gz" "http://downloads.sourceforge.net/project/pyqt/sip/sip-4.18.1/sip-4.18.1.tar.gz"
-tar xzf "${BUILDS_DIR}/sip.tar.gz" -C "${BUILDS_DIR}" --keep-newer-files
-cd "${BUILDS_DIR}/sip-4.18.1"
-python configure.py
-make
-sudo make install
-
-# Install PyQt5
-curl -L -o "${BUILDS_DIR}/pyqt.tar.gz" "http://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.7/PyQt5_gpl-5.7.tar.gz"
-tar xzf "${BUILDS_DIR}/pyqt.tar.gz" -C "${BUILDS_DIR}" --keep-newer-files
-cd "${BUILDS_DIR}/PyQt5_gpl-5.7"
-python configure.py --confirm-license --no-designer-plugin -e QtCore -e QtGui -e QtWidgets -e QtPrintSupport -e QtXml -e QtTest -e QtNetwork
-make
-sudo make install
-
-# Install PIP packages
-pip install -U pip
-pip install -U cython==0.23.4
-pip install -U verlib==0.1
-pip install -U mockito-without-hardcoded-distribute-version==0.5.4
-pip install -U nose==1.3.7
-pip install -U nose-cov==1.6
-pip install -U coveralls
-pip install -e hg+https://danielepantaleone@bitbucket.org/danielepantaleone/cx_freeze/@ubuntu#egg=cx_Freeze
-pip install -e git+https://github.com/danielepantaleone/pyjnius.git@i386#egg=pyjnius --exists-action i
-
-cd ~
-rm -rf "${BUILDS_DIR}"
+class BuiltInActionsTestCase(EddyTestCase):
+    """
+    Tests for the palette plugin.
+    """
+    #############################################
+    #   CUT / COPY / PASTE
+    #################################
