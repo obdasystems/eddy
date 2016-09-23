8c839f99e4a784753e03b9e99087d433d3c69321
danielepantaleone <danielepantaleone@me.com>
2016-09-23 13:31:06 +0200

    tests: added tests for equivalence assertion axioms

--- tests/test_profiles.py
+++ tests/test_profiles.py
@@ -46,7 +46,7 @@ from eddy.core.functions.path import expandPath
 from tests import EddyTestCase
 
 
-class ProfilesTestCase(EddyTestCase):
+class OWL2ProfileTestCase(EddyTestCase):
     """
     Tests for Eddy profiles.
     """
@@ -54,7 +54,7 @@ class ProfilesTestCase(EddyTestCase):
         """
         Initialize test case environment.
         """
-        super(ProfilesTestCase, self).setUp()
+        super(OWL2ProfileTestCase, self).setUp()
         self.init('test_project_2')
         self.session.project.profile.reset = Mock()
 
@@ -300,4 +300,218 @@ class ProfilesTestCase(EddyTestCase):
         # THEN
         self.assertEqual(len(self.project.edges()), num_edges_in_project)
         self.assertEqual(self.project.profile.pvr().message(), 'Role chain nodes cannot be target of a Role inclusion')
+        self.assertFalse(self.project.profile.pvr().isValid())
+
+    #############################################
+    #   EQUIVALENCE
+    #################################
+
+    def test_equivalence_no_graphol_expression(self):
+        # GIVEN
+        self.__give_focus_to_diagram('@tests/.tests/test_project_2/diagram1.graphol')
+        num_edges_in_project = len(self.project.edges())
+        # WHEN
+        self.__insert_edge_between(Item.EquivalenceEdge, (Item.IndividualNode, 'I1'), (Item.IndividualNode, 'I2'))
+        # THEN
+        self.assertEqual(len(self.project.edges()), num_edges_in_project)
+        self.assertEqual(self.project.profile.pvr().message(),'Type mismatch: equivalence must involve two graphol expressions')
+        self.assertFalse(self.project.profile.pvr().isValid())
+
+    def test_equivalence_between_concept_and_role(self):
+        # GIVEN
+        self.__give_focus_to_diagram('@tests/.tests/test_project_2/diagram1.graphol')
+        num_edges_in_project = len(self.project.edges())
+        # WHEN
+        self.__insert_edge_between(Item.EquivalenceEdge, (Item.ConceptNode, 'C1'), (Item.RoleNode, 'R1'))
+        # THEN
+        self.assertEqual(len(self.project.edges()), num_edges_in_project)
+        self.assertEqual(self.project.profile.pvr().message(), 'Type mismatch: equivalence between Concept and Role')
+        self.assertFalse(self.project.profile.pvr().isValid())
+
+    def test_equivalence_between_concept_and_attribute(self):
+        # GIVEN
+        self.__give_focus_to_diagram('@tests/.tests/test_project_2/diagram1.graphol')
+        num_edges_in_project = len(self.project.edges())
+        # WHEN
+        self.__insert_edge_between(Item.EquivalenceEdge, (Item.ConceptNode, 'C1'), (Item.AttributeNode, 'A1'))
+        # THEN
+        self.assertEqual(len(self.project.edges()), num_edges_in_project)
+        self.assertEqual(self.project.profile.pvr().message(), 'Type mismatch: equivalence between Concept and Attribute')
+        self.assertFalse(self.project.profile.pvr().isValid())
+
+    def test_equivalence_between_concept_and_value_domain(self):
+        # GIVEN
+        self.__give_focus_to_diagram('@tests/.tests/test_project_2/diagram1.graphol')
+        num_edges_in_project = len(self.project.edges())
+        # WHEN
+        self.__insert_edge_between(Item.EquivalenceEdge, (Item.ConceptNode, 'C1'), (Item.ValueDomainNode, 'xsd:string'))
+        # THEN
+        self.assertEqual(len(self.project.edges()), num_edges_in_project)
+        self.assertEqual(self.project.profile.pvr().message(), 'Type mismatch: equivalence between Concept and Value Domain')
+        self.assertFalse(self.project.profile.pvr().isValid())
+
+    def test_equivalence_between_role_and_attribute(self):
+        # GIVEN
+        self.__give_focus_to_diagram('@tests/.tests/test_project_2/diagram1.graphol')
+        num_edges_in_project = len(self.project.edges())
+        # WHEN
+        self.__insert_edge_between(Item.EquivalenceEdge, (Item.RoleNode, 'R1'), (Item.AttributeNode, 'A1'))
+        # THEN
+        self.assertEqual(len(self.project.edges()), num_edges_in_project)
+        self.assertEqual(self.project.profile.pvr().message(), 'Type mismatch: equivalence between Role and Attribute')
+        self.assertFalse(self.project.profile.pvr().isValid())
+
+    def test_equivalence_between_role_and_value_domain(self):
+        # GIVEN
+        self.__give_focus_to_diagram('@tests/.tests/test_project_2/diagram1.graphol')
+        num_edges_in_project = len(self.project.edges())
+        # WHEN
+        self.__insert_edge_between(Item.EquivalenceEdge, (Item.RoleNode, 'R1'), (Item.ValueDomainNode, 'xsd:string'))
+        # THEN
+        self.assertEqual(len(self.project.edges()), num_edges_in_project)
+        self.assertEqual(self.project.profile.pvr().message(), 'Type mismatch: equivalence between Role and Value Domain')
+        self.assertFalse(self.project.profile.pvr().isValid())
+
+    def test_equivalence_between_attribute_and_value_domain(self):
+        # GIVEN
+        self.__give_focus_to_diagram('@tests/.tests/test_project_2/diagram1.graphol')
+        num_edges_in_project = len(self.project.edges())
+        # WHEN
+        self.__insert_edge_between(Item.EquivalenceEdge, (Item.AttributeNode, 'A1'), (Item.ValueDomainNode, 'xsd:string'))
+        # THEN
+        self.assertEqual(len(self.project.edges()), num_edges_in_project)
+        self.assertEqual(self.project.profile.pvr().message(), 'Type mismatch: equivalence between Attribute and Value Domain')
+        self.assertFalse(self.project.profile.pvr().isValid())
+
+    def test_equivalence_between_role_and_union_node(self):
+        # GIVEN
+        self.__give_focus_to_diagram('@tests/.tests/test_project_2/diagram1.graphol')
+        num_edges_in_project = len(self.project.edges())
+        target = first(filter(lambda x: x.type() is Item.UnionNode, self.project.nodes(self.session.mdi.activeDiagram())))
+        # WHEN
+        self.__insert_edge_between(Item.EquivalenceEdge, (Item.RoleNode, 'R1'), target)
+        # THEN
+        self.assertEqual(len(self.project.edges()), num_edges_in_project)
+        self.assertEqual(self.project.profile.pvr().message(), 'Type mismatch: role node and union node are not compatible')
+        self.assertFalse(self.project.profile.pvr().isValid())
+
+    def test_equivalence_between_role_and_disjoint_union_node(self):
+        # GIVEN
+        self.__give_focus_to_diagram('@tests/.tests/test_project_2/diagram1.graphol')
+        num_edges_in_project = len(self.project.edges())
+        target = first(filter(lambda x: x.type() is Item.DisjointUnionNode, self.project.nodes(self.session.mdi.activeDiagram())))
+        # WHEN
+        self.__insert_edge_between(Item.EquivalenceEdge, (Item.RoleNode, 'R1'), target)
+        # THEN
+        self.assertEqual(len(self.project.edges()), num_edges_in_project)
+        self.assertEqual(self.project.profile.pvr().message(), 'Type mismatch: role node and disjoint union node are not compatible')
+        self.assertFalse(self.project.profile.pvr().isValid())
+
+    def test_equivalence_between_role_and_intersection_node(self):
+        # GIVEN
+        self.__give_focus_to_diagram('@tests/.tests/test_project_2/diagram1.graphol')
+        num_edges_in_project = len(self.project.edges())
+        target = first(filter(lambda x: x.type() is Item.IntersectionNode, self.project.nodes(self.session.mdi.activeDiagram())))
+        # WHEN
+        self.__insert_edge_between(Item.EquivalenceEdge, (Item.RoleNode, 'R1'), target)
+        # THEN
+        self.assertEqual(len(self.project.edges()), num_edges_in_project)
+        self.assertEqual(self.project.profile.pvr().message(), 'Type mismatch: role node and intersection node are not compatible')
+        self.assertFalse(self.project.profile.pvr().isValid())
+
+    def test_equivalence_between_attribute_and_union_node(self):
+        # GIVEN
+        self.__give_focus_to_diagram('@tests/.tests/test_project_2/diagram1.graphol')
+        num_edges_in_project = len(self.project.edges())
+        target = first(filter(lambda x: x.type() is Item.UnionNode, self.project.nodes(self.session.mdi.activeDiagram())))
+        # WHEN
+        self.__insert_edge_between(Item.EquivalenceEdge, (Item.AttributeNode, 'A1'), target)
+        # THEN
+        self.assertEqual(len(self.project.edges()), num_edges_in_project)
+        self.assertEqual(self.project.profile.pvr().message(), 'Type mismatch: attribute node and union node are not compatible')
+        self.assertFalse(self.project.profile.pvr().isValid())
+
+    def test_equivalence_between_attribute_and_disjoint_union_node(self):
+        # GIVEN
+        self.__give_focus_to_diagram('@tests/.tests/test_project_2/diagram1.graphol')
+        num_edges_in_project = len(self.project.edges())
+        target = first(filter(lambda x: x.type() is Item.DisjointUnionNode, self.project.nodes(self.session.mdi.activeDiagram())))
+        # WHEN
+        self.__insert_edge_between(Item.EquivalenceEdge, (Item.AttributeNode, 'A1'), target)
+        # THEN
+        self.assertEqual(len(self.project.edges()), num_edges_in_project)
+        self.assertEqual(self.project.profile.pvr().message(), 'Type mismatch: attribute node and disjoint union node are not compatible')
+        self.assertFalse(self.project.profile.pvr().isValid())
+
+    def test_equivalence_between_attribute_and_intersection_node(self):
+        # GIVEN
+        self.__give_focus_to_diagram('@tests/.tests/test_project_2/diagram1.graphol')
+        num_edges_in_project = len(self.project.edges())
+        target = first(filter(lambda x: x.type() is Item.IntersectionNode, self.project.nodes(self.session.mdi.activeDiagram())))
+        # WHEN
+        self.__insert_edge_between(Item.EquivalenceEdge, (Item.AttributeNode, 'A1'), target)
+        # THEN
+        self.assertEqual(len(self.project.edges()), num_edges_in_project)
+        self.assertEqual(self.project.profile.pvr().message(), 'Type mismatch: attribute node and intersection node are not compatible')
+        self.assertFalse(self.project.profile.pvr().isValid())
+
+    def test_equivalence_between_value_domain_expressions(self):
+        # GIVEN
+        self.__give_focus_to_diagram('@tests/.tests/test_project_2/diagram1.graphol')
+        num_edges_in_project = len(self.project.edges())
+        target = first(filter(lambda x: x.type() is Item.DatatypeRestrictionNode, self.project.nodes(self.session.mdi.activeDiagram())))
+        # WHEN
+        self.__insert_edge_between(Item.EquivalenceEdge, (Item.ValueDomainNode, 'xsd:string'), target)
+        # THEN
+        self.assertEqual(len(self.project.edges()), num_edges_in_project)
+        self.assertEqual(self.project.profile.pvr().message(), 'Type mismatch: equivalence between value-domain expressions')
+        self.assertFalse(self.project.profile.pvr().isValid())
+
+    def test_equivalence_between_complement_node_and_role(self):
+        # GIVEN
+        self.__give_focus_to_diagram('@tests/.tests/test_project_2/diagram1.graphol')
+        num_edges_in_project = len(self.project.edges())
+        source = first(filter(lambda x: x.type() is Item.ComplementNode, self.project.nodes(self.session.mdi.activeDiagram())))
+        # WHEN
+        self.__insert_edge_between(Item.EquivalenceEdge, source, (Item.RoleNode, 'R1'))
+        # THEN
+        self.assertEqual(len(self.project.edges()), num_edges_in_project)
+        self.assertEqual(self.project.profile.pvr().message(), 'Equivalence is forbidden when expressing role disjointness')
+        self.assertFalse(self.project.profile.pvr().isValid())
+
+    def test_equivalence_between_complement_node_and_attribute(self):
+        # GIVEN
+        self.__give_focus_to_diagram('@tests/.tests/test_project_2/diagram1.graphol')
+        num_edges_in_project = len(self.project.edges())
+        source = first(filter(lambda x: x.type() is Item.ComplementNode, self.project.nodes(self.session.mdi.activeDiagram())))
+        # WHEN
+        self.__insert_edge_between(Item.EquivalenceEdge, source, (Item.AttributeNode, 'A1'))
+        # THEN
+        self.assertEqual(len(self.project.edges()), num_edges_in_project)
+        self.assertEqual(self.project.profile.pvr().message(), 'Equivalence is forbidden when expressing attribute disjointness')
+        self.assertFalse(self.project.profile.pvr().isValid())
+
+    def test_equivalence_between_role_chain_node_and_role_chain_node(self):
+        # GIVEN
+        self.__give_focus_to_diagram('@tests/.tests/test_project_2/diagram1.graphol')
+        num_edges_in_project = len(self.project.edges())
+        source = first(filter(lambda x: x.type() is Item.RoleChainNode, self.project.nodes(self.session.mdi.activeDiagram())))
+        target = first(filter(lambda x: x.type() is Item.RoleChainNode and x is not source, self.project.nodes(self.session.mdi.activeDiagram())))
+        # WHEN
+        self.__insert_edge_between(Item.EquivalenceEdge, source, target)
+        # THEN
+        self.assertEqual(len(self.project.edges()), num_edges_in_project)
+        self.assertEqual(self.project.profile.pvr().message(), 'Equivalence is forbidden in presence of a role chain node')
+        self.assertFalse(self.project.profile.pvr().isValid())
+
+    def test_equivalence_between_role_and_role_chain_node(self):
+        # GIVEN
+        self.__give_focus_to_diagram('@tests/.tests/test_project_2/diagram1.graphol')
+        num_edges_in_project = len(self.project.edges())
+        target = first(filter(lambda x: x.type() is Item.RoleChainNode, self.project.nodes(self.session.mdi.activeDiagram())))
+        # WHEN
+        self.__insert_edge_between(Item.EquivalenceEdge, (Item.RoleNode, 'R1'), target)
+        # THEN
+        self.assertEqual(len(self.project.edges()), num_edges_in_project)
+        self.assertEqual(self.project.profile.pvr().message(), 'Equivalence is forbidden in presence of a role chain node')
         self.assertFalse(self.project.profile.pvr().isValid())
