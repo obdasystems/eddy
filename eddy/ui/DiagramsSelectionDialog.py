ffda074f4f1b20723ae7074e4e48fddfbbef26a1
ashwingoldfish <ashwingoldfish@yahoo.co.in>
2018-05-12 22:53:08 +0200

    Version 1.1 9th commit with changes to the diagram selection dialog. scroll function cannot be enabled.

--- eddy/ui/DiagramsSelectionDialog.py
+++ eddy/ui/DiagramsSelectionDialog.py
@@ -134,8 +134,8 @@ class DiagramsSelectionDialog(QtWidgets.QDialog, HasThreadingSystem, HasWidgetSy
         #self.setFixedSize(max(DiagramsGroup.width(),400),DiagramsGroup.height()+100)
         #self.setMaximumHeight(400)
         #self.setMinimumHeight(400)
-        self.setFixedHeight(400)
-        self.setFixedWidth(600)
+        self.setFixedHeight(200)
+        self.setFixedWidth(400)
 
         self.setFont(Font('Roboto', 12))
         self.setWindowIcon(QtGui.QIcon(':/icons/128/ic_eddy'))
