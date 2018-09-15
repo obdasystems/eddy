237c439c7e8f14502b157db9d0613c2f5ce2746f
Manuel Namici <manuel.namici@gmail.com>
2018-09-15 17:20:29 +0200

    examples: remove unexistent tests example

--- eddy/core/application.py
+++ eddy/core/application.py
@@ -124,7 +124,6 @@ class Eddy(QtWidgets.QApplication):
             expandPath('@examples/Family'),
             expandPath('@examples/LUBM'),
             expandPath('@examples/Pizza'),
-            expandPath('@examples/tests'),
         ]
 
         if not settings.contains('project/recent'):
