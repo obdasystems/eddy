76068f9fcc175707c303c703b07f5db77db562ff
Manuel Namici <manuel.namici@gmail.com>
2019-07-02 17:26:39 +0200

    regex: improved owl name validation regexp
    Closes #33

--- eddy/core/regex.py
+++ eddy/core/regex.py
@@ -34,7 +34,21 @@
 
 
 import re
+import textwrap
 
+# RFC 3987 character classes
+ucschar = textwrap.dedent(
+    r'\xA0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF'
+    r'\U00010000-\U0001FFFD\U00020000-\U0002FFFD'
+    r'\U00030000-\U0003FFFD\U00040000-\U0004FFFD'
+    r'\U00050000-\U0005FFFD\U00060000-\U0006FFFD'
+    r'\U00070000-\U0007FFFD\U00080000-\U0008FFFD'
+    r'\U00090000-\U0009FFFD\U000A0000-\U000AFFFD'
+    r'\U000B0000-\U000BFFFD\U000C0000-\U000CFFFD'
+    r'\U000D0000-\U000DFFFD\U000E1000-\U000EFFFD'
+)
+ireserved = r'\uE000-\uF8FF\U000F0000-\U000FFFFD\U00100000-\U0010FFFD'
+iunreserved = r'a-zA-Z0-9\-._~{}'.format(ucschar)
 
 RE_CAMEL_SPACE = re.compile(r'([a-z])([A-Z])') # space string on camel case token
 RE_CARDINALITY = re.compile(r'^\(\s*(?P<min>[\d-]+)\s*,\s*(?P<max>[\d-]+)\s*\)$') # parse cardinality restriction
@@ -44,7 +58,7 @@ RE_FACET = re.compile(r'^(?P<facet>[\w:]*)[\s\^]*"(?P<value>.*)"$') # tokenize f
 RE_ITEM_PREFIX = re.compile(r'^(?P<prefix>[^\d])(?P<value>\d+)$') # split items prefix/id
 RE_LOG_MESSAGE = re.compile(r'^(?P<date>.{10})\s(?P<time>.{8})\s(?P<level>\w+)\s+(?P<message>.*)$') # tokenize log messages
 RE_QUOTED = re.compile(r'^".*"$') # identify strings fully embraced into quotes
-RE_OWL_INVALID_CHAR = re.compile(r'[\W]') # identify OWL invalid characters
+RE_OWL_INVALID_CHAR = re.compile(r'[^{}]'.format(iunreserved)) # identify OWL invalid characters
 RE_OWL_ONTOLOGY_FUNCTIONAL_TAG = re.compile(r'^Ontology\s*\(.*$') # identify OWL ontology tag in Functional OWL
 RE_OWL_ONTOLOGY_MANCHESTER_TAG = re.compile(r'^Ontology:\s*.*$') # identify OWL ontology tag in Mancherster OWL
 RE_OWL_ONTOLOGY_TURTLE_TAG = re.compile(r'^.*owl:Ontology.*$') # identify OWL ontology tag in Turtle OWL
