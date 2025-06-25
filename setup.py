0b644f03e8ddbc3a83b147db557a8ee5462ef404
Manuel Namici <manuel.namici@gmail.com>
2025-06-25 16:29:56 +0200

    setup: add python 3.13 to the supported python versions

--- setup.py
+++ setup.py
@@ -601,6 +601,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Utilities',
     ],
