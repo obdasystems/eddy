17f676c810ba2c64f6bb898581c3c3a1bdee67cb
MariaRosaria <45686128+MariaRosariaFraraccio@users.noreply.github.com>
2025-07-21 16:59:20 +0200

    ontology-manager: on dbl click edit annotation

--- eddy/ui/ontology.py
+++ eddy/ui/ontology.py
@@ -460,6 +460,7 @@ class OntologyManagerDialog(QtWidgets.QDialog, HasWidgetSystem):
         table.verticalHeader().setSectionsClickable(False)
         table.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
         #table.setSelectionMode(QAbstractItemView.MultiSelection)
+        connect(table.cellDoubleClicked, self.editAnnotationAssertion)
         self.addWidget(table)
 
         selectBtn = QtWidgets.QPushButton('Select All', objectName = 'annotation_assertions_selectall_button')
